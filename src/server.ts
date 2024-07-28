1c7bcee23d2821c53b17edc49cc76d7793566e43
Felipe Amaral <felipeamaraldev15@gmail.com>
2024-07-27 23:38:33 -0300

    configuracao de ambiente de desenvolvimento

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import express, {Request, Response} from 'express'
+import express, { Request, Response } from 'express'
 
 export const app = express()
 
 app.listen(3330, () => {
-  console.log("Server is running")
+  console.log('Server is running')
 })
 
-app.get('/', (req:Request, res:Response) => {
-    
-    return res.status(200).send({message: 'hello world'})
+app.use(express.json())
 
+app.get('/', (req: Request, res: Response) => {
+  return res.status(200).send({ message: 'hello world' })
 })
